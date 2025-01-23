3df8b284529e317a26e8c013ed690e6372ee89ca
sajid6230 <sajid6230@gmail.com>
2025-01-23 10:53:30 -0600

    11th Problem

---
+++ 11.py
@@ -0,0 +1,13 @@
+#How do you sort an array of integers in ascending & descending order?
+
+
+list_1 = [21,2334,65457,58,67962,342,4564,23423,567]
+
+print("The given array : ", list_1)
+
+list_1.sort()
+print("Sorted array in ascending order : ",list_1)
+
+
+list_1.sort(reverse= True)
+print("Sorted array in descending order : ",list_1)
