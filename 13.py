65deb2951529bbf75b85865227d1769c4717cd91
sajid6230 <sajid6230@gmail.com>
2025-01-23 20:01:15 -0600

    13th Problem

---
+++ 13.py
@@ -0,0 +1,11 @@
+#How do you calculate the sum of two integers? (function)
+
+def add_2_int(a,b):
+    print(f"Given number is '{a}' and '{b}'")
+    sum = a + b
+    
+    return(sum)
+
+
+result = add_2_int(5,6)
+print("Sum of 2 given interger is : ", result)
